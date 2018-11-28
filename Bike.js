d9d5ab2ee5cee9946db24358d34dd73c97f8550c
Eran Geva <>
2018-11-28 18:48:37 +0200

    merge positions

--- Bike.js
+++ Bike.js
@@ -4,17 +4,64 @@ export class Bike {
         return '#' + [(~~(Math.random() * 16)).toString(16), (~~(Math.random() * 16)).toString(16), (~~(Math.random() * 16)).toString(16)].join('');
     }
 
-    createMarker(latlng) {
-        return L.marker(latlng, {
-            // icon: L.icon({ 'marker-color': randomColor() })
-        }).bindPopup("Clicked at " + latlng.toString()).addTo(this.map).openPopup();
-        // }).bindPopup('<input type="text" id="message" /><br /><button id="add-button">Add</button>').addTo(this.map).openPopup();
+    // createMarker(latlng) {
+    //     return L.circleMarker(latlng, {
+    //         // icon: L.icon({ 'marker-color': randomColor() })
+    //     }).bindPopup("Clicked at " + latlng.toString()).addTo(this.map).openPopup();
+    //     // }).bindPopup('<input type="text" id="message" /><br /><button id="add-button">Add</button>').addTo(this.map).openPopup();
+    // }
+
+    clickMap(e) {
+        this.fire.push({ latlng: e.latlng, num: 1 });
+    }
+
+    dist(p, q) {
+        var dlat = p.latlng.lat - q.latlng.lat;
+        var dlng = p.latlng.lng - q.latlng.lng;
+        return Math.sqrt(dlat * dlat + dlng * dlng);
+    }
+
+    mergePositions(positions) {
+        var ret = [];
+        positions.forEach((p) => {
+            var found = ret.find((q) => this.dist(p, q) < 0.0005);
+            if (found) {
+                ++found.num;
+                found.latlng.lat = (found.latlng.lat + p.latlng.lat) / 2.0;
+                found.latlng.lng = (found.latlng.lng + p.latlng.lng) / 2.0;
+            } else {
+                ret.push(p);
+            }
+        });
+        return ret;
+    }
+
+    fireValues(snapshot) {
+        // Clean
+        this.features.forEach(m => {
+            this.map.removeLayer(m);
+        });
+
+        // Flatten
+        var positions = [];
+        for (var k in snapshot.val()) {
+            positions.push(snapshot.val()[k]);
+        }
+
+        // Merge
+        var positions = this.mergePositions(positions);
+
+        // Show and save
+        this.features = positions.map((p) => {
+            return L.circleMarker(p.latlng, {weight: p.num}).addTo(this.map);
+        });
     }
 
     init() {
         // Initialize Firebase
         var config = { apiKey: "AIzaSyAD1oxPPPZRb64TJcADtRENkWS5ZcrtJ3Y", authDomain: "bikewarn-223907.firebaseapp.com", databaseURL: "https://bikewarn-223907.firebaseio.com", projectId: "bikewarn-223907", storageBucket: "bikewarn-223907.appspot.com", messagingSenderId: "638058417911" };
         firebase.initializeApp(config);
+        this.fire = firebase.database().ref('pos');
 
         this.map = L.map('map', { attributionControl: true, zoomControl: true, });
         this.map.setView(new L.LatLng(32.07, 34.78), 15);
@@ -23,21 +70,9 @@ export class Bike {
 
         this.features = [];
 
-        this.map.on('click', (e) => {
-            firebase.database().ref('pos').push(L.marker(e.latlng, {}).toGeoJSON());
-        });
+        this.map.on('click', this.clickMap.bind(this));
 
-        firebase.database().ref('pos').on('value', (snapshot) => {
-            this.features.forEach(m => {
-                this.map.removeLayer(m);
-            });
-
-            this.features.length = 0;
-            for (var k in snapshot.val()) {
-                var m = L.geoJSON(snapshot.val()[k]).addTo(this.map);
-                this.features.push(m);
-            }
-        });
+        this.fire.on('value', this.fireValues.bind(this));
 
     }
 
