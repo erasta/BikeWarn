54c107886b6fe09d910230029e565146aa0d2ddf
Eran Geva <>
2018-11-28 18:57:09 +0200

    showing current position

--- Bike.js
+++ Bike.js
@@ -53,7 +53,7 @@ export class Bike {
 
         // Show and save
         this.features = positions.map((p) => {
-            return L.circleMarker(p.latlng, {weight: p.num}).addTo(this.map);
+            return L.circleMarker(p.latlng, { weight: p.num }).addTo(this.map);
         });
     }
 
@@ -64,9 +64,12 @@ export class Bike {
         this.fire = firebase.database().ref('pos');
 
         this.map = L.map('map', { attributionControl: true, zoomControl: true, });
-        this.map.setView(new L.LatLng(32.07, 34.78), 15);
         var tileurl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
         L.tileLayer(tileurl, { id: 'mapbox.light', attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>' }).addTo(this.map);
+
+        this.map.on('locationerror', () => { this.map.setView(new L.LatLng(32.07, 34.78), 15); });
+        this.map.on('locationfound', (e) => { L.marker(e.latlng).addTo(this.map); });
+        this.map.locate({ setView: true, maxZoom: 18 });
 
         this.features = [];
 
